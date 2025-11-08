e32b85a012695088df6aaa525d30a76ae43d1fba
apros7 <lucas.vilsen@gmail.com>
2025-11-08 16:05:31 +0100

    change to 8000

--- api.py
+++ api.py
@@ -282,5 +282,5 @@ async def analyze_video_base64(request: Base64VideoRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
